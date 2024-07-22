bf7c24f2893adcd44ef6a4a159d2c49aff6c6c34
Dac Haven <d.dachaven@gmail.com>
2024-07-22 15:18:54 +0100

    Add the function to compile the shopping list
    The function will
    - compile the list if the add item is clicked or the enter key is pressed after input
    - if the item is already on the list, it will alert the user
    - else, if the item is not in the list, it appends the item

--- shoppingList.js
+++ shoppingList.js
@@ -1,12 +1,41 @@
 const container = document.querySelector("div");
 
-const ListItemInput = document.querySelector("#item");
+let ListItemInput = document.querySelector("#item");
 ListItemInput.focus();
 
 const addItemBtn = document.querySelector("button");
-const listItems = document.querySelector("ul");
+let listItems = document.querySelector("ul");
 
 addItemBtn.addEventListener("click", () => {
-  let items = ListItemInput.value;
-  console.log(items);
+  let ListItemInputValue = ListItemInput.value.trim();
+
+  let item = document.createElement("li");
+  const itemSpan = document.createElement("span");
+  const itemDeleteBtn = document.createElement("button");
+
+  item.append(itemSpan, itemDeleteBtn);
+
+  if (ListItemInputValue !== "") {
+    const existingItem = Array.from(listItems.children).find(
+      (item) => item.querySelector("span").textContent === ListItemInputValue
+    );
+    {
+      if (existingItem) {
+        alert("Item already exists!");
+      } else {
+        itemSpan.textContent += ListItemInputValue;
+        itemDeleteBtn.textContent = "Delete";
+
+        listItems.appendChild(item);
+      }
+    }
+  }
+
+  ListItemInput.value = "";
+});
+
+ListItemInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addItemBtn.click();
+  }
 });
